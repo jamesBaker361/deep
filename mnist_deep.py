5dc932e490f08d0c70262af9aff345ca8161a3ae
James Baker <jlbaker361@gmail.com>
2025-05-14 14:27:20 -0400

    wal clock

--- mnist_deep.py
+++ mnist_deep.py
@@ -74,7 +74,7 @@ model.eval()
 correct = 0
 with torch.no_grad():
     for x, y in test_loader:
-        x, y = x.to(device), y.to(device)
+        x, y = x.to(device,torch_dtype), y.to(device)
         preds = model(x).argmax(dim=1)
         correct += (preds == y).sum().item()
 
